b569783c5ee2d6628a649bddaf5c3f296191c05a
Ken Ng <kenngsimply@gmail.com>
2023-01-23 13:26:48 +0800

    update: manifest v2 to v3

--- init.js
+++ init.js
@@ -1,17 +1,3 @@
-// placeholder - will be used when options are implemented
+chrome.bookmarks.onCreated.addListener(function (id, node) {});
 
-chrome.runtime.onStartup.addListener(function() {
-  registerEvents();
-});
-
-
-function registerEvents() {
-
-  chrome.bookmarks.onCreated.addListener( function(id, node) {
-  })
-
-  chrome.tabs.onActivated.addListener(function( info ) {
-  })
-
-
-}
+chrome.tabs.onActivated.addListener(function (info) {});
